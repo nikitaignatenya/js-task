aeb15656750a81edcb60ddb795c2993f288974f1
Nikita Ignatenya <ihnatenyawork@gmail.com>
2025-06-23 14:22:37 +0300

    JS-error

--- 28. regExp/15/app.js
+++ 28. regExp/15/app.js
@@ -26,24 +26,45 @@ const arr = [
   "Y",
   "Z",
 ];
-function check(checkArr, newArr) {
+// function check(checkArr, newArr) {
+//   try {
+//     for (let i = 0; i < checkArr.length; i++) {
+//       arr.forEach((el, l) => {
+//         if (!isNaN(el)) throw new Error("Интервал содержит числа");
+//         el === checkArr[i]
+//           ? newArr.push(arr[l + 1].toUpperCase())
+//             ? el.toLowerCase() == checkArr[i]
+//             : newArr.push(arr[l + 1].toLowerCase())
+//           : null;
+//       });
+//     }
+//     newArr.slice(0, newArr.length - 1).forEach((el) => {
+//       !checkArr.includes(el) ? console.log(el) : null;
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// check(["V", "W", "Y"], []);
+
+function check(array) {
   try {
-    for (let i = 0; i < checkArr.length; i++) {
-      arr.forEach((el, l) => {
-        if (!isNaN(el)) throw new Error("Массив содержит числа");
-        el === checkArr[i]
-          ? newArr.push(arr[l + 1].toUpperCase())
-            ? el.toLowerCase() == checkArr[i]
-            : newArr.push(arr[l + 1].toLowerCase())
-          : null;
-      });
-    }
-    newArr.slice(0, newArr.length - 1).forEach((el) => {
-      !checkArr.includes(el) ? console.log(el) : null;
+    arr.forEach((el) => {
+      if (!isNaN(el)) throw new Error("Число");
+    });
+
+    let newArr = arr.slice(
+      arr.indexOf(array[0].toUpperCase()),
+      arr.indexOf(array[array.length - 1].toUpperCase()) + 1
+    );
+    console.log(newArr);
+    newArr.forEach((el) => {
+      !array.includes(el) ? console.log(el) : null;
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-check(["O", "Q", "R", "S"], []);
+check(["T", "U", "V", "W", "X", "Z"]);
