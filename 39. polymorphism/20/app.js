6b7d501fdfb94b1e16b3bbf7ae9163ebd84bda7c
Nikita Ignatenya <ihnatenya.work@gmail.com>
2025-08-05 10:09:40 +0300

    simple numbers

--- 39. polymorphism/20/app.js
+++ 39. polymorphism/20/app.js
@@ -3,20 +3,39 @@
 // Создай наследника PrimeSquares, метод get(n) возвращает квадраты этих чисел.
 // Пример: get(4) → [4, 9, 25, 49].
 
+// class PrimeGenerator {
+//   get(n) {
+//     let arr = [];
+//     let count = 2;
+//     while (arr.length < n) {
+//       if (this.isSimpleNumber(count) == true) {
+//         arr.push(count);
+//       }
+//       count++;
+//     }
+
+//     return arr;
+//   }
+//   isSimpleNumber(n) {
+//     for (let i = 2; i <= n / 2; i++) {
+//       if (n % i === 0) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// class PrimeSquares extends PrimeGenerator {
+//   get(n) {
+//     let arr = super.get(n);
+//     const res = arr.map((el) => el ** 2);
+//     return res;
+//   }
+// }
+
 class PrimeGenerator {
   get(n) {
-    let arr = [];
-    let count = 2;
-    while (arr.length < n) {
-      if (this.isSimpleNumber(count) == true) {
-        arr.push(count);
-      }
-      count++;
-    }
-
-    return arr;
-  }
-  isSimpleNumber(n) {
     for (let i = 2; i <= n / 2; i++) {
       if (n % i === 0) {
         return false;
@@ -28,16 +47,19 @@ class PrimeGenerator {
 
 class PrimeSquares extends PrimeGenerator {
   get(n) {
-    let arr = super.get(n);
+    let arr = [];
+    let count = 2;
+    while (arr.length < n) {
+      if (super.get(count) == true) {
+        arr.push(count);
+      }
+      count++;
+    }
     const res = arr.map((el) => el ** 2);
     return res;
   }
 }
 
-// const primeSquares = new PrimeSquares();
-// const finalRes = primeSquares.get(5);
-// console.log(finalRes);
-
-// const primeSquares = new PrimeSquares();
-// primeSquares.get(4);
-
+const primeSquares = new PrimeSquares();
+const res = primeSquares.get(5);
+console.log(res);
